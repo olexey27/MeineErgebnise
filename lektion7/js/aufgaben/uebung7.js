d74e74441670dae45c25bd92a6756cd3bbb75ced
Alexey <allxyog@gmail.com>
2022-05-10 11:55:01 +0200

    shuuuu

--- lektion7/js/aufgaben/uebung7.js
+++ lektion7/js/aufgaben/uebung7.js
@@ -6,99 +6,127 @@
  * 1) Kreiere einen neuen Apfel
  */
 function aufgabe1() {
-  console.debug("Aufgabe 1 ==========================")
+    console.debug("Aufgabe 1 ==========================")
 
-  class Apfel {
-    constructor (farbe) {
-      this.farbe = farbe
+    class Apfel {
+        constructor(farbe) {
+            this.farbe = farbe
+        }
+
     }
 
-  }
+    //ToDo: füge ab hier deinen Code ein
 
-  //ToDo: füge ab hier deinen Code ein
+    let apfel = new Apfel("rot")
 
-  console.debug(apfel.farbe)
+    console.debug(apfel.farbe)
 }
 
 /**
  * 2) Schreibe deine Methode
  */
 function aufgabe2() {
-  console.debug("Aufgabe 2 ==========================")
+    console.debug("Aufgabe 2 ==========================")
 
-  class Auto {
-    constructor (farbe) {
-      this.farbe = farbe
+    class Auto {
+        constructor(farbe) {
+            this.farbe = farbe
+        }
+
+        //ToDo: füge ab hier deinen Code ein
+
+        hupen() {
+            console.debug("honk honk")
+        }
     }
 
-    //ToDo: füge ab hier deinen Code ein
-  }
+    let bmw = new Auto("rot")
 
-  let bmw = new Auto("rot")
-
-  bmw.hupen()
+    bmw.hupen()
 }
 
 /**
  * 3) Rufe die Farbe und die Methode auf
  */
 function aufgabe3() {
-  console.debug("Aufgabe 3 ==========================");
+    console.debug("Aufgabe 3 ==========================");
 
-  class Pkw {
-    constructor (farbe) {
-      this.farbe = farbe
+    class Pkw {
+        constructor(farbe) {
+            this.farbe = farbe
+        }
+
+        hupen() {
+            console.debug("honk honk")
+        }
     }
 
-    hupen() {
-      console.debug("honk honk")
-    }
-  }
+    let mini = new Pkw("blau")
 
-  let mini = new Pkw("blau")
+    //ToDo: füge ab hier deinen Code ein
 
-  //ToDo: füge ab hier deinen Code ein
+    console.debug(mini.farbe)
+
+    mini.hupen()
 }
 
 /**
  * 4) Schreibe den Konstruktor
  */
 function aufgabe4() {
-  console.debug("Aufgabe 4 ==========================");
+    console.debug("Aufgabe 4 ==========================");
 
-  class Motorrad {
-    //ToDo: füge ab hier deinen Code ein
+    class Motorrad {
+        //ToDo: füge ab hier deinen Code ein
 
-    hupen() {
-      console.debug("honk honk")
+        constructor(baujahr) {
+            this.baujahr = baujahr
+        }
+
+        hupen() {
+            console.debug("honk honk")
+        }
     }
-  }
 
-  let harley = new Motorrad(1980)
+    let harley = new Motorrad(1980)
 
-  console.debug(harley.baujahr)
+    console.debug(harley.baujahr)
 }
 
 /**
  * 5) Schreibe eine komplette Klasse und erstelle den Tesla
  */
 function aufgabe5() {
-  console.debug("Aufgabe 5 ==========================");
+    console.debug("Aufgabe 5 ==========================");
 
-  //ToDo: füge ab hier deinen Code ein
+    //ToDo: füge ab hier deinen Code ein
 
-  console.debug(tesla.farbe)
-  tesla.hupen()
+    class Elektroauto {
+        constructor(farbe) {
+            this.farbe = farbe
+        }
+
+        hupen() {
+            console.debug("honk honk")
+        }
+    }
+
+    let tesla = new Elektroauto("grau")
+
+    console.debug(tesla.farbe)
+
+    tesla.hupen()
 }
 
 /**
  * Hauptfunktion, in der die Aufgaben aufgerufen werden
  */
 function main() {
-  aufgabe1()
-  aufgabe2()
-  aufgabe3()
-  aufgabe4()
-  aufgabe5()
+    aufgabe1()
+    aufgabe2()
+    aufgabe3()
+    aufgabe4()
+    aufgabe5()
 }
+
 main()
