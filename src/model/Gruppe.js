f65637cf3a38409ca589fd0feef5a4b8bb6bb940
Alexey <allxyog@gmail.com>
2022-05-11 16:58:09 +0200

    Wird Bearbeitet

--- src/model/Gruppe.js
+++ src/model/Gruppe.js
@@ -1,4 +1,5 @@
 import Artikel from "./Artikel.js"
+import artikel from "./Artikel.js";
 
 /**
  * Klasse zum Gruppieren der Artikel
@@ -10,57 +11,81 @@ import Artikel from "./Artikel.js"
  * @property {Artikel[]} artikelListe - Liste der Artikel in dieser Gruppe
  */
 class Gruppe {
-  static counter = 1
-  id = Gruppe.counter++
-  index
-  name
-  artikelListe = []
+    static counter = 1
+    id = Gruppe.counter++
+    index
+    name
+    artikelListe = []
 
-  constructor(name, index) {
-    this.name = name
-    this.index = index
-  }
+    constructor(name, index) {
+        this.name = name
+        this.index = index
+    }
 
-  /**
-   * Sucht einen Artikel anhand seines Namens
-   * @param {String} suchName - Name des gesuchten Artikels
-   * @param {Boolean} meldungAusgeben - steuert, ob eine Meldung ausgegeben wird
-   * @returns {Artikel|null}
-   */
-  artikelFinden(suchName, meldungAusgeben) {
-    // TODO: füge hier Deinen Code ein
+    /**
+     * Sucht einen Artikel anhand seines Namens
+     * @param {String} suchName - Name des gesuchten Artikels
+     * @param {Boolean} meldungAusgeben - steuert, ob eine Meldung ausgegeben wird
+     * @returns {Artikel|null}
+     */
+    artikelFinden(suchName, meldungAusgeben) {
+        // TODO: füge hier Deinen Code ein
 
-  }
+        for (let artikel of this.artikelListe) {
+            if (artikel.name == suchName) {
+                return artikel
+            }
+        }
+        if (meldungAusgeben) {
+            console.warn("artikel nicht gefunden ", suchName)
+        }
+        return null
+    }
 
-  /**
-   * Listet die Artikel in dieser Gruppe in der Konsole auf
-   * @param {Boolean} gekauft - steuert die Anzeige der gekauften oder noch zu kaufenden Artikel
-   */
-  artikelAuflisten(gekauft) {
-    // TODO: füge hier Deinen Code ein
+    /**
+     * Listet die Artikel in dieser Gruppe in der Konsole auf
+     * @param {Boolean} gekauft - steuert die Anzeige der gekauften oder noch zu kaufenden Artikel
+     */
+    artikelAuflisten(gekauft) {
+        // TODO: füge hier Deinen Code ein
 
-  }
+    }
 
-  /**
-   * Fügt einen Artikel zur ArtikelListe hinzu und gibt diesen als Wert zurück
-   * @param {String} name - Name des neuen Artikels
-   * @returns {Artikel} neuerArtikel - der neu erzeugte Artikel
-   */
-  artikelHinzufuegen(name) {
-    // TODO: doppelte Artikel abfangen!
-    let neuerArtikel = new Artikel(name)
-    this.artikelListe.push(neuerArtikel)
-    return neuerArtikel
-  }
+    /**
+     * Fügt einen Artikel zur ArtikelListe hinzu und gibt diesen als Wert zurück
+     * @param {String} name - Name des neuen Artikels
+     * @returns {Artikel} neuerArtikel - der neu erzeugte Artikel
+     */
+    artikelHinzufuegen(name) {
+        // TODO: doppelte Artikel abfangen!
+        let neuerArtikel = new Artikel(name)
+        this.artikelListe.push(neuerArtikel)
+        return neuerArtikel
+    }
 
-  /**
-   *
-   * @param {String} name
-   */
-  artikelEntfernen(name) {
-    // TODO: Artikel finden, position ermitteln
-    let position = 0;
-    this.artikelListe.splice(position , 1)
-  }
+    /**
+     *
+     * @param {String} name
+     */
+    artikelEntfernen(name) {
+        // TODO: Artikel finden, position ermitteln
+        let position = 0;
+        this.artikelListe.splice(position, 1)
+    }
 
 }
+
+function main() {
+
+    let zahl = 0
+    let gruppe = new Gruppe("produktGruppe1", 0)
+    gruppe.artikelHinzufuegen("artikel")
+    gruppe.artikelHinzufuegen("artikel2")
+    console.debug(gruppe.artikelListe)
+    gruppe.artikelFinden("artikel")
+    gruppe.artikelFinden("xxx", true)
+
+}
+
+main()
+
